1f6c56aa934f57ba474c2aa1178123dcdc44c862
saira-gulamali <sguacamole98@gmail.com>
2024-06-21 16:37:04 +0100

    fix bug for express-rate-limit

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.set("trust proxy", 1); //enable if you're behind a reverse proxy (Heroku, Bl
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    validate: true,
   })
 );
 app.use(express.json());
